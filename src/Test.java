ed1bcd4135fa540423dbef9779c9f6ae75a830fd
liwei <7274561@qq.com>
2020-12-27 11:46:16 +0800

    解决用户名和密码错误重新提示用户输入

--- src/Test.java
+++ src/Test.java
@@ -4,22 +4,26 @@ import java.util.Scanner;
 
 public class Test {
     public static void main(String[] args) throws ClassNotFoundException {
-        System.out.println("请输入用户名：");
-        Scanner sc = new Scanner(System.in);
-        String username = sc.next();//阻塞方法
-        System.out.println("请输入密码：");
-        String password = sc.next();
+        boolean bo=true;
+        while (bo) {
+            System.out.println("请输入用户名：");
+            Scanner sc = new Scanner(System.in);
+            String username = sc.next();//阻塞方法
+            System.out.println("请输入密码：");
+            String password = sc.next();
 
-        //File file = new File("C:\\Users\\Administrator\\IdeaProjects\\ConsoleShop\\src\\users.xlsx");
-        InputStream in = Class.forName("Test").getResourceAsStream("/users.xlsx");
-        ReadExcel readExcel = new ReadExcel();//创建对象
-        User users[] = readExcel.readExcel(in);
-        for (int i = 0; i < users.length; i++) {
-            if (username.equals(users[i].getUsername()) && password.equals(users[i].getPassword())) {
-                System.out.println("登录成功");
-                break;
-            } else {
-                System.out.println("登录失败");
+            //File file = new File("C:\\Users\\Administrator\\IdeaProjects\\ConsoleShop\\src\\users.xlsx");
+            InputStream in = Class.forName("Test").getResourceAsStream("/users.xlsx");
+            ReadExcel readExcel = new ReadExcel();//创建对象
+            User users[] = readExcel.readExcel(in);
+            for (int i = 0; i < users.length; i++) {
+                if (username.equals(users[i].getUsername()) && password.equals(users[i].getPassword())) {
+                    System.out.println("登录成功");
+                    bo=false;
+                    break;
+                } else {
+                    System.out.println("登录失败");
+                }
             }
         }
     }
