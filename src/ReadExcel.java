cc7f71b240e4929bc9538813772716c6ecb1e965
liwei <7274561@qq.com>
2020-12-27 11:34:40 +0800

    修改读取users.xlsx的方式

--- src/ReadExcel.java
+++ src/ReadExcel.java
@@ -7,13 +7,14 @@ import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.text.DecimalFormat;
 
 public class ReadExcel {
-    public User[] readExcel(File file) {
+    public User[] readExcel(InputStream in) {
         User users[] = null;
         try {
-            XSSFWorkbook xw = new XSSFWorkbook(new FileInputStream(file));
+            XSSFWorkbook xw = new XSSFWorkbook(in);
             XSSFSheet xs = xw.getSheetAt(0);
             users = new User[xs.getLastRowNum()];
             for (int j = 1; j <= xs.getLastRowNum(); j++) {
--- src/Test.java
+++ src/Test.java
@@ -1,23 +1,19 @@
 import java.io.File;
+import java.io.InputStream;
 import java.util.Scanner;
 
 public class Test {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ClassNotFoundException {
         System.out.println("请输入用户名：");
-
         Scanner sc = new Scanner(System.in);
         String username = sc.next();//阻塞方法
-        System.out.println("你输入的用户名：" + username);
-
         System.out.println("请输入密码：");
         String password = sc.next();
 
-        System.out.println("你输入的密码是：" + password);
-
-        File file = new File("C:\\Users\\Administrator\\IdeaProjects\\ConsoleShop\\src\\users.xlsx");
+        //File file = new File("C:\\Users\\Administrator\\IdeaProjects\\ConsoleShop\\src\\users.xlsx");
+        InputStream in = Class.forName("Test").getResourceAsStream("/users.xlsx");
         ReadExcel readExcel = new ReadExcel();//创建对象
-        User users[] = readExcel.readExcel(file);
-        System.out.println("密码："+users[0].getPassword());
+        User users[] = readExcel.readExcel(in);
         for (int i = 0; i < users.length; i++) {
             if (username.equals(users[i].getUsername()) && password.equals(users[i].getPassword())) {
                 System.out.println("登录成功");
