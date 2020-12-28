78c5876f082713b10594b1c73f80dba0f4445d1d
liwei <7274561@qq.com>
2020-12-28 19:17:37 +0800

    修改ReadProductExcel

--- src/ReadProductExcel.java
+++ src/ReadProductExcel.java
@@ -9,41 +9,42 @@ import java.io.InputStream;
 import java.text.DecimalFormat;
 
 public class ReadProductExcel {
-    public User[] readExcel(InputStream in) {
-        User users[] = null;
+    public Product[] readExcel(InputStream in) {
+        Product products[] = null;
         try {
             XSSFWorkbook xw = new XSSFWorkbook(in);
             XSSFSheet xs = xw.getSheetAt(0);
-            users = new User[xs.getLastRowNum()];
+            products = new Product[xs.getLastRowNum()];
             for (int j = 1; j <= xs.getLastRowNum(); j++) {
                 XSSFRow row = xs.getRow(j);
-                User user = new User();//每循环一次就把电子表格的一行的数据给对象赋值
+                Product product = new Product();
                 for (int k = 0; k <= row.getLastCellNum(); k++) {
                     XSSFCell cell = row.getCell(k);
                     if (cell == null)
                         continue;
                     if (k == 0) {
-                        user.setUsername(this.getValue(cell));//给username属性赋值
+                        product.setpId(this.getValue(cell));
                     } else if (k == 1) {
-                        user.setPassword(this.getValue(cell));//给password属性赋值
+                        product.setpName(this.getValue(cell));
                     } else if (k == 2) {
-                        user.setAddress(this.getValue(cell));//给address属性赋值
+                        product.setPrice(this.getValue(cell));//把字符串转Float
                     } else if (k == 3) {
-                        user.setPhone(this.getValue(cell));//给phone属性赋值
+                        product.setpDesc(this.getValue(cell));
                     }
                 }
-                users[j-1] = user;
+                products[j - 1] = product;
             }
+
         } catch (IOException e) {
             e.printStackTrace();
         }
-        return users;
+        return products;
     }
 
     private String getValue(XSSFCell cell) {
         String value;
         CellType type = cell.getCellTypeEnum();
-        DecimalFormat df = new DecimalFormat("#");
+
         switch (type) {
             case STRING:
                 value = cell.getStringCellValue();
@@ -55,8 +56,9 @@ public class ReadProductExcel {
                 value = cell.getBooleanCellValue() + "";
                 break;
             case NUMERIC:
-                value = df.format(cell.getNumericCellValue());//double和一个空字符串相连接，最终得到字符串
-                System.out.println("转换后的："+value);
+                DecimalFormat df = new DecimalFormat("#");
+                value = df.format(cell.getNumericCellValue());
+                System.out.println("处理后的：" + value);
                 break;
             case FORMULA:
                 value = cell.getCellFormula();
