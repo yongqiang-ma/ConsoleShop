01c3b15bc1a3aaabfb2587ea73999feb87ae61f5
liwei <7274561@qq.com>
2020-12-28 20:32:20 +0800

    1、显示商品信息 2、加入购物车

--- src/ReadProductExcel.java
+++ src/ReadProductExcel.java
@@ -9,7 +9,8 @@ import java.io.InputStream;
 import java.text.DecimalFormat;
 
 public class ReadProductExcel {
-    public Product[] readExcel(InputStream in) {
+
+    public Product[] getAllProduct(InputStream in) {
         Product products[] = null;
         try {
             XSSFWorkbook xw = new XSSFWorkbook(in);
@@ -39,6 +40,38 @@ public class ReadProductExcel {
             e.printStackTrace();
         }
         return products;
+    }
+    public Product getProductById(String id,InputStream in) {
+        Product products[] = null;
+        try {
+            XSSFWorkbook xw = new XSSFWorkbook(in);
+            XSSFSheet xs = xw.getSheetAt(0);
+            for (int j = 1; j <= xs.getLastRowNum(); j++) {
+                XSSFRow row = xs.getRow(j);
+                Product product = new Product();
+                for (int k = 0; k <= row.getLastCellNum(); k++) {
+                    XSSFCell cell = row.getCell(k);
+                    if (cell == null)
+                        continue;
+                    if (k == 0) {
+                        product.setpId(this.getValue(cell));
+                    } else if (k == 1) {
+                        product.setpName(this.getValue(cell));
+                    } else if (k == 2) {
+                        product.setPrice(this.getValue(cell));//把字符串转Float
+                    } else if (k == 3) {
+                        product.setpDesc(this.getValue(cell));
+                    }
+                }
+                if(id.equals(product.getpId())){
+                    return product;
+                }
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 
     private String getValue(XSSFCell cell) {
--- src/Test.java
+++ src/Test.java
@@ -1,9 +1,10 @@
+import java.io.IOException;
 import java.io.InputStream;
 import java.util.Scanner;
 
 public class Test {
-    public static void main(String[] args) throws ClassNotFoundException {
-        boolean bo=true;
+    public static void main(String[] args) throws ClassNotFoundException, IOException {
+        boolean bo = true;
         while (bo) {
             System.out.println("请输入用户名：");
             Scanner sc = new Scanner(System.in);
@@ -13,12 +14,69 @@ public class Test {
 
             //File file = new File("C:\\Users\\Administrator\\IdeaProjects\\ConsoleShop\\src\\users.xlsx");
             InputStream in = Class.forName("Test").getResourceAsStream("/users.xlsx");
+            InputStream inProduct = Class.forName("Test").getResourceAsStream("/product.xlsx");
             ReadUserExcel readExcel = new ReadUserExcel();//创建对象
             User users[] = readExcel.readExcel(in);
             for (int i = 0; i < users.length; i++) {
                 if (username.equals(users[i].getUsername()) && password.equals(users[i].getPassword())) {
                     System.out.println("登录成功");
-                    bo=false;
+                    bo = false;
+                    /*
+                    显示商品
+                     */
+                    ReadProductExcel readProductExcel = new ReadProductExcel();
+                    Product products[] = readProductExcel.getAllProduct(inProduct);
+                    for (Product product : products) {
+                        System.out.print(product.getpId());
+                        System.out.print("\t" + product.getpName());
+                        System.out.print("\t" + product.getPrice());
+                        System.out.println("\t" + product.getpDesc());
+                    }
+                    System.out.println("请输入商品ID把该商品加入购物车");
+                    String pId = sc.next();
+                    int count = 0;
+                    /*
+                    创建一个购物车的数组：存的是商品
+                     */
+                    Product carts[] = new Product[3];
+                    /*
+                    根据此ID去Excel中去查找是否有该ID的商品信息，如果有则返回该商品即可
+                     */
+                    inProduct = null;
+                    inProduct = Class.forName("Test").getResourceAsStream("/product.xlsx");
+                    Product product = readProductExcel.getProductById(pId, inProduct);
+                    System.out.println("要购买的商品价格：" + product.getPrice());
+                    if(product!=null){
+                        carts[count++]=product;
+                    }
+                    System.out.println("继续购买商品请按1");
+                    System.out.println("查看购物车请按2");
+                    int choose=sc.nextInt();
+                    if(choose==1){
+                        inProduct = null;
+                        inProduct = Class.forName("Test").getResourceAsStream("/product.xlsx");
+                        readProductExcel = new ReadProductExcel();
+                        products = readProductExcel.getAllProduct(inProduct);
+                        for (Product p : products) {
+                            System.out.print(p.getpId());
+                            System.out.print("\t" + p.getpName());
+                            System.out.print("\t" + p.getPrice());
+                            System.out.println("\t" + p.getpDesc());
+                        }
+                        System.out.println("请输入商品ID把该商品加入购物车");
+                        pId = sc.next();
+                        inProduct = null;
+                        inProduct = Class.forName("Test").getResourceAsStream("/product.xlsx");
+                        product = readProductExcel.getProductById(pId, inProduct);
+                        System.out.println("要购买的商品价格：" + product.getPrice());
+                        if(product!=null){
+                            carts[count++]=product;
+                        }
+                    }else if(choose==2){
+                        /*
+                        查看购物车
+                         */
+                    }
                     break;
                 } else {
                     System.out.println("登录失败");
