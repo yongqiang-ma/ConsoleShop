6845e6238e25c1fae83e541e12254536f6b33a94
liwei <7274561@qq.com>
2020-12-29 21:10:57 +0800

    显示商品信息、添加商品到购物车

--- src/ReadProductExcel.java
+++ src/ReadProductExcel.java
@@ -9,7 +9,12 @@ import java.io.InputStream;
 import java.text.DecimalFormat;
 
 public class ReadProductExcel {
-    public Product[] readExcel(InputStream in) {
+    /**
+     * 返回所有的商品
+     * @param in
+     * @return 包含了所有商品的数组
+     */
+    public Product[] getAllProduct(InputStream in) {
         Product products[] = null;
         try {
             XSSFWorkbook xw = new XSSFWorkbook(in);
@@ -39,6 +44,44 @@ public class ReadProductExcel {
             e.printStackTrace();
         }
         return products;
+    }
+
+    /**
+     * 根据ID查找商品
+     * @param id
+     * @param in
+     * @return 返回id对应的商品
+     */
+    public Product getProductById(String id,InputStream in) {
+        try {
+            XSSFWorkbook xw = new XSSFWorkbook(in);
+            XSSFSheet xs = xw.getSheetAt(0);
+            for (int j = 1; j <= xs.getLastRowNum(); j++) {
+                XSSFRow row = xs.getRow(j);
+                Product product = new Product();
+                for (int k = 0; k <= row.getLastCellNum(); k++) {
+                    XSSFCell cell = row.getCell(k);
+                    if (cell == null)
+                        continue;
+                    if (k == 0) {
+                        product.setId(this.getValue(cell));
+                    } else if (k == 1) {
+                        product.setName(this.getValue(cell));
+                    } else if (k == 2) {
+                        product.setPrice(Float.valueOf(this.getValue(cell)));//把字符串转Float
+                    } else if (k == 3) {
+                        product.setDesc(this.getValue(cell));
+                    }
+                }
+                if(id.equals(product.getId())){
+                    return product;
+                }
+                //如果id（手动输入的）和product的id（从电子表格里读出来的）一致，则表示找到了该商品，然后返回该商品即可
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;//如果找不到就返回空
     }
 
     private String getValue(XSSFCell cell) {
--- src/Test.java
+++ src/Test.java
@@ -1,3 +1,5 @@
+import javafx.scene.control.Tab;
+
 import java.io.InputStream;
 import java.util.Scanner;
 
@@ -17,15 +19,42 @@ public class Test {
 
             //File file=new File("C:\\Users\\Administrator\\IdeaProjects\\ConsoleShop\\src\\users.xlsx");
             InputStream in = Class.forName("Test").getResourceAsStream("/users.xlsx");//  /表示的就是classpath
+
+            InputStream inPro = Class.forName("Test").getResourceAsStream("/product.xlsx");//  /表示的就是classpath
+
             ReadUserExcel readExcel = new ReadUserExcel();//创建对象
             User users[] = readExcel.readExcel(in);
             for (int i = 0; i < users.length; i++) {
                 if (username.equals(users[i].getUsername()) && password.equals(users[i].getPassword())) {
+                    bool=false;
                     /*
                     显示商品的信息
                      */
-
-                    bool=false;
+                    ReadProductExcel readProductExcel=new ReadProductExcel();
+                    Product products[]=readProductExcel.getAllProduct(inPro);
+                    for(Product product:products){
+                        System.out.print(product.getId());
+                        System.out.print("\t"+product.getName());
+                        System.out.print("\t\t"+product.getPrice());
+                        System.out.println("\t\t"+product.getDesc());
+                    }
+                    /*
+                    遍历数组
+                     */
+                    int count=0;
+                    Product productes[]=new Product[3];//创建购物车（用数组模拟）
+                    System.out.println("请输入商品ID，把该商品加入购物车：");
+                    String pId=sc.next();
+                    ReadProductExcel readProductExcel1=new ReadProductExcel();
+                    inPro=null;
+                    inPro = Class.forName("Test").getResourceAsStream("/product.xlsx");//  /表示的就是classpath
+                    Product product=readProductExcel1.getProductById(pId,inPro);
+                    if(product!=null){
+                        /*
+                        把商品加入购物车
+                         */
+                        productes[count++]=product;
+                    }
                     break;
                 } else {
                     System.out.println("登录失败");
